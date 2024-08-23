c20632e041c68df2b12387421d17bf3c43f00800
n00bNn00b <dewanreal@gmail.com>
2024-08-24 03:34:29 +0600

    modal updated

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,31 +1,31 @@
+import { AlertDialog } from "@/components/ui/alert-dialog";
+import Modal from "@/components/ui/apps/topbar/Modal/Modal";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-;import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Services } from "@/types/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
-  
+
 const Home = () => {
   const [services, setServices] = useState<Services[]>([]);
   const url = import.meta.env.VITE_API_URL;
-  useEffect(()=> {
+  useEffect(() => {
     const fetchServices = async () => {
       try {
-        const response = await axios.get<Services[]>(
-          `${url}/services`
-        );
+        const response = await axios.get<Services[]>(`${url}/services`);
         setServices(response.data);
       } catch (error) {
         console.log(error);
@@ -38,40 +38,66 @@ const Home = () => {
     <>
       <Card className="bg-white/80">
         <CardHeader>
-            <CardTitle className="text-xl font-bold font-special text-Green-100">CHT Disaster Updates</CardTitle>
-            <CardDescription className="text-Blue-200">A list of vlounteer groups and working areas to accelerate the working progress</CardDescription>
+          <CardTitle className="text-xl font-bold font-special text-Green-100">
+            CHT Disaster Updates
+          </CardTitle>
+          <CardDescription className="text-Blue-200">
+            A list of vlounteer groups and working areas to accelerate the
+            working progress
+          </CardDescription>
         </CardHeader>
+        <AlertDialog defaultOpen={true}>
+          <Modal />
+        </AlertDialog>
         <CardContent>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                    <TableHead className="text-Green-200">Team Name</TableHead>
-                    <TableHead className="text-Green-200">Status</TableHead>
-                    <TableHead className="text-Green-200">Working Area</TableHead>
-                    <TableHead className="text-Green-200">Provided Services</TableHead>
-                    <TableHead className="text-Green-200">Contact No.</TableHead>
-                    <TableHead className="text-Green-200">Date</TableHead>
-                    <TableHead className="text-Green-200">Admin Status</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {services.map(service => (
-                      <TableRow key={service._id}>
-                        <TableCell className="text-Blue-200">{service.teamName}</TableCell>
-                        <TableCell className="text-Blue-200">{service.status}</TableCell>
-                        <TableCell className="text-Blue-200">{service.workingArea}</TableCell>
-                        <TableCell className="text-Blue-200">{service.providedService}</TableCell>
-                        <TableCell className="text-Blue-200">{service.contact}</TableCell>
-                        <TableCell className="text-Blue-200">{service.date}</TableCell>
-                        <TableCell className="text-Blue-200">{service.isVerifiedByAdmin? "Verified by IAU": "Still not verified"}</TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-    </CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="text-Green-200">Team Name</TableHead>
+                <TableHead className="text-Green-200">Status</TableHead>
+                <TableHead className="text-Green-200">Working Area</TableHead>
+                <TableHead className="text-Green-200">
+                  Provided Services
+                </TableHead>
+                <TableHead className="text-Green-200">Contact No.</TableHead>
+                <TableHead className="text-Green-200">Date</TableHead>
+                <TableHead className="text-Green-200">Admin Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {services.map((service) => (
+                <TableRow key={service._id}>
+                  <TableCell className="text-Blue-200">
+                    {service.teamName}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.status}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.workingArea}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.providedService}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.contact}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.date}
+                  </TableCell>
+                  <TableCell className="text-Blue-200">
+                    {service.isVerifiedByAdmin
+                      ? "Verified by IAU"
+                      : "Still not verified"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
       </Card>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
