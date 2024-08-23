4bbaec2a5ccf3f8ecc493b9931f76bd60b6a3ee6
Jyanial Dewan <djyanial@gmail.com>
2024-08-24 04:30:24 +0600

    affected areas functionality added

--- client/src/pages/Submit/Submit.tsx
+++ client/src/pages/Submit/Submit.tsx
@@ -5,7 +5,7 @@ const Submit = () => {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <NavLink to={'/submit/team-progress'} className={({isActive}) => isActive? "underline text-white font-special font-bold" : "text-white font-special font-bold hover:underline"}>Team Progress</NavLink>
+        <NavLink to={'/submit/team-progress'} className={({isActive}) => isActive? "underline text-white font-special font-bold" : "text-white font-special font-bold hover:underline"}>Submit Team Progress</NavLink>
         <NavLink to={'/submit/affected-areas'} className={({isActive}) => isActive? "underline text-white font-special font-bold" : "text-white font-special font-bold hover:underline"}>Submit Affected Areas</NavLink>
       </div>
       <div className="mt-4">
