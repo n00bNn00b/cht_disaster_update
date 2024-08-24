4632c96b68561030f3fa9f5efe9fa2567589fa83
n00bNn00b <dewanreal@gmail.com>
2024-08-24 14:25:24 +0600

    bengali content added

--- client/src/pages/Submit/Submit.tsx
+++ client/src/pages/Submit/Submit.tsx
@@ -1,18 +1,35 @@
-import { NavLink, Outlet } from "react-router-dom"
-
+import { NavLink, Outlet } from "react-router-dom";
 
 const Submit = () => {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <NavLink to={'/submit/team-progress'} className={({isActive}) => isActive? "underline text-white font-special font-bold" : "text-white font-special font-bold hover:underline"}>Team Progress</NavLink>
-        <NavLink to={'/submit/affected-areas'} className={({isActive}) => isActive? "underline text-white font-special font-bold" : "text-white font-special font-bold hover:underline"}>Submit Affected Areas</NavLink>
+        <NavLink
+          to={"/submit/team-progress"}
+          className={({ isActive }) =>
+            isActive
+              ? "underline text-white font-special font-bold"
+              : "text-white font-special font-bold hover:underline"
+          }
+        >
+          টিমের কার্যক্রমের অগ্রগতি
+        </NavLink>
+        <NavLink
+          to={"/submit/affected-areas"}
+          className={({ isActive }) =>
+            isActive
+              ? "underline text-white font-special font-bold"
+              : "text-white font-special font-bold hover:underline"
+          }
+        >
+          দুর্গত এলাকার নাম তালিকায় হালনাগাদ করুন
+        </NavLink>
       </div>
       <div className="mt-4">
-        <Outlet/> 
+        <Outlet />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Submit
+export default Submit;
